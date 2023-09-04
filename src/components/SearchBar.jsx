372556fa73747488cd70d2a09da6c244cf6fa53f
MaximilianoDienes <maxdienes@gmail.com>
2023-09-04 13:07:33 -0300

    mi primer commit!

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+export default function SearchBar(props) {
+   // const {onSearch} = props;
+
+   return (
+      <div>
+         <input type='search'/>
+         <button onClick={props.onSearch}>Agregar</button>
+      </div>
+   );
+}
